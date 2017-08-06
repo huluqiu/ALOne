00f7208bfa829709bf42799cc551c9681728c285
翻滚的葫芦球 <yongle.lin@outlook.com>
2017-08-06 21:50:43 +0800

    feat: create iOS project

---
+++ ALOne/ALOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALOne
+//
+//  Created by 林永乐 on 2017/8/6.
+//  Copyright © 2017年 alezai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
